585e0353587ab938c6f5b87c3791b76cb55b0235
Jordy Contreras <jcontrerasm10@miumg.edu.gt>
2026-09-23 14:39:45 -0600

    Add Cita class fields and constructors

--- agenda-citas-core/src/main/java/edu/umg/programacion2/proyecto/modelo/Cita.java
+++ agenda-citas-core/src/main/java/edu/umg/programacion2/proyecto/modelo/Cita.java
@@ -34,3 +34,58 @@ public class Cita {
         this.duracionMinutos = duracionMinutos;
         this.estado = estado;
     }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public String getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(String servicio) {
+        this.servicio = servicio;
+    }
+
+    public int getDuracionMinutos() {
+        return duracionMinutos;
+    }
+
+    public void setDuracionMinutos(int duracionMinutos) {
+        this.duracionMinutos = duracionMinutos;
+    }
+
+    public EstadoCita getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoCita estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Cita{id=" + id + ", cliente='" + cliente + "', fechaHora=" + fechaHora
+                + ", servicio='" + servicio + "', duracionMinutos=" + duracionMinutos
+                + ", estado=" + estado + '}';
+    }
+}
